2c2b96350a0fc8eb101088f6b99e3e6b4f1b392f
Moniphal Say <moniphal@locationd.net>
2016-12-09 23:27:15 +0530

    use config file

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
+DEBUG = False
 SQLALCHEMY_DATABASE_URL = "sqlite://item_catalog.db"
+SECRET_KEY = 'Sm9obiBTY2hyb20ga2lja3MgYXNz'
 #SQLALCHEMY_DATABASE_URL = 
 #                      "postgresql://postgres:postgres@localhost/item_catalog.db"
 
--- item_catalog.py
+++ item_catalog.py
@@ -4,6 +4,7 @@ from flask import Flask, session
 from flask import request, redirect, url_for, render_template
 
 app = Flask(__name__, template_folder='templates')
+app.config.from_object('config')
 
 # Decorator that route "/" and "/item_catalog" with below function
 @app.route("/")
@@ -13,7 +14,8 @@ def index():
 
 
 def main():
-    app.run(host='0.0.0.0', debug=True)
+    app.secret_key = app.config['SECRET_KEY']
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
 
 if __name__ == '__main__':
