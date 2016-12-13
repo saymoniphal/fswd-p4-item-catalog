e36d159116069593f7511f725e4242e9f3a70699
Moniphal Say <moniphal@locationd.net>
2016-12-13 14:26:28 +0530

    working on adding third party authentication google signin

--- run.py
+++ run.py
@@ -6,12 +6,22 @@ from views import *
 
 app = Flask(__name__, template_folder='templates')
 app.config.from_object('config')
+app.register_blueprint(category_app)
 
 # Decorator that route "/" and "/item_catalog" with below function
 @app.route("/")
 @app.route("/item_catalog")
 def index():
     return render_template("index.html")
+
+
+@app.route('/login')
+def login():
+    # generate random string of length 32 to be used as session state
+    state = ''.join(random.choice(string.ascii_uppercase + string.digits)
+                    for x in xrange(32)) 
+    login_session['state'] = state
+    return render_template('login.html', STATE=state)
 
 
 def main():
